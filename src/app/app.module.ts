bd1f84c91b77108d13b99f9ea1955f2093cf0285
unknown <rommel.sanzonetti@correo.unimet.edu.ve>
2020-12-05 14:16:21 -0400

    conexion con API lista

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,27 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { HttpClientModule } from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { EditTaskComponent } from './pages/edit-task/edit-task.component';
-import {NavbarComponent} from '../app/componentes/navbar/navbar.component'
+import {NavbarComponent} from '../app/componentes/navbar/navbar.component';
+import { ListaPersonajesComponent } from './lista-personajes/lista-personajes.component'
+import { AngularFireModule } from '@angular/fire';
+import { environment } from 'src/environments/environment';
 @NgModule({
   declarations: [
     AppComponent,
     EditTaskComponent,
-    NavbarComponent
+    NavbarComponent,
+    ListaPersonajesComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    HttpClientModule,
+    AppRoutingModule,
+
+    AngularFireModule.initializeApp(environment.firebaseConfig)
+
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/personajes-api.service.ts
@@ -0,0 +1,67 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from "@angular/common/http";
+import { environment } from 'src/environments/environment';
+import { Personaje } from './personaje';
+import { templateJitUrl } from '@angular/compiler';
+import firebase from "firebase/app";
+import { AngularFireDatabase } from '@angular/fire/database';
+@Injectable({
+  providedIn: 'root'
+})
+export class PersonajesAPIService {
+
+  constructor(private http: HttpClient,
+    private db: AngularFireDatabase) { }
+
+
+  searchCharacters(query = '', page = 1) {
+    const filter = `${environment.baseUrlAPI}/?name=${query}&page=${page}`;
+    return this.http.get<Personaje[]>(filter);
+
+  }
+
+  getDetails(id: number) {
+
+    return this.http.get<Personaje>(`${environment.baseUrlAPI}/${id}`);
+
+  }
+
+  async addToWL(character: Personaje, user: firebase.User) {
+
+    let item$ = this.db.list("/users/" + user.uid + "/wish-list/");
+
+    let wLKey = item$.push({
+      //  POR AHORA BORRAR
+    }).key
+    console.log(character.key);
+    this.db.object("/users/" + user.uid + "/favoritos/" + "/characters/" + character.id)
+      .set({
+        character
+      })
+
+
+
+  }
+
+
+  create(character) {
+    return this.db.list("/characters").push(character);
+  }
+
+  getAll() {
+    return this.db.list("/characters");
+  }
+
+  getProduct(characterId) {
+    return this.db.object("/characters/" + characterId);
+  }
+
+  update(characterId, character) {
+    return this.db.object("/characters/" + characterId).update(character);
+  }
+
+  delete(characterId) {
+    return this.db.object("/characters/" + characterId).remove();
+  }
+
+}
